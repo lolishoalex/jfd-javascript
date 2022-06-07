2a023177abe37939c67027c54c2ed1eecc1c79ec
lolishoalex <shapilovka@gmail.com>
2022-06-07 19:39:14 +0200

    01 task 04 confirm

---
+++ 01-baseJS/task-04/index.js
@@ -0,0 +1,8 @@
+confirm('JavaScript появился в 1995 году?');
+alert('Верно');
+
+confirm('Спецификация JavaScript называется ECMAScript?');
+alert('Верно');
+
+confirm('JavaScript был создан за 1 месяц?');
+alert('JavaScript был разработан за 10 дней');
