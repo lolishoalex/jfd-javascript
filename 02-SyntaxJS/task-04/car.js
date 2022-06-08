c1cb90b156ac2ba8bf38447ef9be34d97afd7123
lolishoalex <shapilovka@gmail.com>
2022-06-08 19:24:45 +0200

    02 task 04 changing variables

---
+++ 02-SyntaxJS/task-04/car.js
@@ -0,0 +1,19 @@
+const carName = 'Lada';
+
+let maxCarSpeed = '10 mph';
+
+maxCarSpeed = '50 mph';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '35 mph';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '75 mph';
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+let carOwner = 'Vasya';
+
+carOwner = 'Luda';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Petya';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Gosha';
+console.log('У машины изменился владелец', carOwner);
