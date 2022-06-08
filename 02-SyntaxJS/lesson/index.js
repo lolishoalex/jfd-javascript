5232cad8cc74ab025c63369e5aadaaaa0f4a1ef5
lolishoalex <shapilovka@gmail.com>
2022-06-08 18:04:21 +0200

    02 syntax coments

--- 02-SyntaxJS/lesson/index.js
+++ 02-SyntaxJS/lesson/index.js
@@ -4,6 +4,17 @@ console.log('Hello World1'); // работает
 
 console.log('Hello World2') // тоже работает 
 
-// ПРОБЕЛЫ в  JS не учитываются
+// ПРОБЕЛЫ - в JS не учитываются
          console.log('Hello World3');
     console.log('Hello World4');
+
+// КОММЕНТАРИИ 2 ТИПА // - ОДНА СТРОКА, /**/ - БЛОК СТРОК
+
+// console.log('Hello World5');
+
+
+/* console.log('Hello World5');
+console.log('Hello World5');
+console.log('Hello World5');
+console.log('Hello World5');
+console.log('Hello World5');*/
