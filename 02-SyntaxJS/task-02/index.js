f07eb4ba5cf9e31209a322428123432f0f182a38
lolishoalex <shapilovka@gmail.com>
2022-06-08 19:00:58 +0200

    02 task 02 creation variables

---
+++ 02-SyntaxJS/task-02/index.js
@@ -0,0 +1,10 @@
+const myName = 'Olga';
+console.log(myName);
+const mySurname = 'Shpylova';
+console.log(mySurname);
+let myFavoriteDrink = 'Coffee';
+console.log(myFavoriteDrink);
+let myFavoriteAnimal = 'Cat';
+console.log(myFavoriteAnimal);
+let myFavoriteProgrammingLanguage = 'JavaScript';
+console.log(myFavoriteProgrammingLanguage);
