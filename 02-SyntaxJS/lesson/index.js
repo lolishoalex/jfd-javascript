44d2d4904aa0e106d557ee07c0507d80aee96e6e
lolishoalex <shapilovka@gmail.com>
2022-06-08 18:00:35 +0200

    02 syntax пробелы

--- 02-SyntaxJS/lesson/index.js
+++ 02-SyntaxJS/lesson/index.js
@@ -1,6 +1,9 @@
 // ; не имеет значения, но нужно выбрать вариант с ней или без нее
 
-console.log('Hello World'); // работает 
+console.log('Hello World1'); // работает 
 
-console.log('Hello World') // тоже работает 
+console.log('Hello World2') // тоже работает 
 
+// ПРОБЕЛЫ в  JS не учитываются
+         console.log('Hello World3');
+    console.log('Hello World4');
