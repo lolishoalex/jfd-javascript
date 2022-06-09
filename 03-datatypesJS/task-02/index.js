a6a5e86ae591e8e932fc16ff5a057e9ab2842745
lolishoalex <shapilovka@gmail.com>
2022-06-09 18:32:39 +0200

    03 task 02

---
+++ 03-datatypesJS/task-02/index.js
@@ -0,0 +1,11 @@
+let lessonJavaScriptNumber = 3;
+lessonJavaScriptNumber = 'three'; 
+alert(lessonJavaScriptNumber);
+
+let lessonJavaScriptTheme = 'Data types';
+lessonJavaScriptTheme = 3;
+alert(lessonJavaScriptTheme);
+
+let lessonJavaScriptHomework = true;
+lessonJavaScriptHomework = null;
+alert(lessonJavaScriptHomework);
