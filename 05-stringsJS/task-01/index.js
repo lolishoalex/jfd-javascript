cf996ca73353660ad52fba71a9bf46e53114d0b7
lolishoalex <shapilovka@gmail.com>
2022-06-13 19:16:04 +0200

    05 task 01 strings

---
+++ 05-stringsJS/task-01/index.js
@@ -0,0 +1,10 @@
+const myName = 'Olga';
+const programmingLanguage  = 'JavaScript';
+const courseCreatorName  = 'Vladilena';
+const reasonText  = 'это мое развитие';
+const numberOfMonth  = '12';
+
+const allInfo = `Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}.
+
+Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев. Я уверен(а), что пройду данный курс до конца!`;
+console.log(allInfo);
