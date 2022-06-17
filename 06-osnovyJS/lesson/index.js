508a490914a988e7c394c68597f78ae38079a237
lolishoalex <shapilovka@gmail.com>
2022-06-17 17:07:01 +0200

    06 osnovyJS maths

---
+++ 06-osnovyJS/lesson/index.js
@@ -0,0 +1,31 @@
+// Математика в JS
+
+// Отрицательные числа
+
+const counter = -10;
+console.log('counter', counter);
+
+// Математические выражения 
+
+console.log('+', 2 + 2); // сложение
+console.log('-', 10 - 2); // вычитание
+console.log('*', 2 * 2); // умножение
+console.log('/', 10 / 5); // деление
+
+console.log(2 + 2 * 2); // сложное выражение, объединение нескольких операторов
+console.log('**', 5 ** 3); // возведение в степень
+console.log('%', 20 % 3); // остаток от деления
+
+// инкремент и декримент
+
+let cupsOfcoffee = 1;
+// cupsOfcoffee = cupsOfcoffee + 1; это выражение можно оптимизировать
+cupsOfcoffee += 2; // или cupsOfcoffee = cupsOfcoffee + 2
+cupsOfcoffee -= 1; // или cupsOfcoffee = cupsOfcoffee - 1
+
+console.log('cupsOfcoffee', cupsOfcoffee);
+
+let value = 2;
+value *= 5; // или value = value * 5
+value /= 2; // или value = value / 2
+console.log('value', value);
