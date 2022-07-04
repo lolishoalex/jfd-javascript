68289bed0429fb5f044476c0e619da4454b69265
lolishoalex <shapilovka@gmail.com>
2022-07-04 18:17:25 +0200

    09-arrays

---
+++ 09-arrays/lesson/index.js
@@ -0,0 +1,14 @@
+// Что такое массивы - это набор элементов
+// Массивы позволяют хранить множестов значений одного либо разных типов
+
+
+// Создание массива
+const salariesOfDevelopers = [400, 500, 600, 2000, 350]
+console.log('salariesOfDevelopers', salariesOfDevelopers)
+// значенниям перечисленным в массиве присваивается индэкс (начинается с 0), у массива обязательно есть длина
+
+// вывод длины массива
+console.log('salariesOfDevelopers.length', salariesOfDevelopers.length)
+
+const names = ['Olga', 'Sahsa', 'Kim']
+console.log('names', names)
