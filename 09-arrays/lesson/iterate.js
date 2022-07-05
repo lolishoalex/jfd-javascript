c1ce4ff30feec63d1bfe1b44ec50a1bc3a7f9641
lolishoalex <shapilovka@gmail.com>
2022-07-05 16:20:23 +0200

    09-arrays-iterate map, filter, find, findIndex, some, every

--- 09-arrays/lesson/iterate.js
+++ 09-arrays/lesson/iterate.js
@@ -28,3 +28,105 @@ developerNames.forEach((name, index, array) =>{
     //console.log('index', index)
     console.log('array', array)
 })
+
+
+
+//Работа с методами массивов: 
+//map, filter, find, findIndex, some, every
+
+
+//Метод map - принимает в себя колл-бэк
+//Используется для того чтобы применить какое-то действие для КАЖДОГО элемента массива
+//метод возвращает новый массив, чтобы увидеть изменения нужно создать новую переменную
+
+
+const salariesOfDevelopers = [400, 500, 600, 2000, 350]
+
+/*const updatedSalaries = salariesOfDevelopers.map((salary, index, array) => {
+    return salary * 2//умножаем на 2
+})
+console.log('updatedSalaries', updatedSalaries)*/
+
+const updatedSalaries = salariesOfDevelopers.map((salary, index, array) => {
+    return salary ** 2//возводим в квадрат
+})
+console.log('updatedSalaries', updatedSalaries)
+
+//Метод filter - принимает в себя колл-бэк
+//метод возвращает новый массив, чтобы увидеть изменения нужно создать новую переменную
+
+const filteredSalaries = salariesOfDevelopers.filter((salary, index, array) => {
+    return salary > 500 //возвращаем условие, если результат тру, то элемент будет добавляться в конечный массив, если фолс, то будет пропускаться
+})
+console.log('filteredSalaries', filteredSalaries)
+
+const filteredSalaries1 = salariesOfDevelopers.filter((salary, index, array) => {
+    return index % 2 === 0 //проверяем индэкс на четность
+})
+console.log('filteredSalaries1', filteredSalaries1)
+
+//Метод find - принимает в себя колл-бэк
+//метод ищет элемент в массиве по условию, метод возвращает только первое значение которое было найдено в массиве по условию
+
+const findSalaries = salariesOfDevelopers.find((salary) => {
+    return salary === 500 //возвращаем условие, если результат тру, то элемент будет добавляться в конечный массив, если фолс, то будет пропускаться
+})
+console.log('findSalaries', findSalaries)
+
+const findSalaries1 = salariesOfDevelopers.find((salary) => {
+    return salary > 400 //выведет только первый найденый элемент удовлетворяющий нашему условию, , если не найден ответ то будет undefinde
+})
+console.log('findSalaries1', findSalaries1)
+
+//Метод findIndex - принимает в себя колл-бэк
+//метод ищет индэкс элемента в массиве по условию, метод возвращает только первое значение которое было найдено в массиве по условию
+
+const findIndexSalaries = salariesOfDevelopers.findIndex((salary) => {
+    return salary === 600 //выведет только первый найденый индєкс элемента удовлетворяющий нашему условию, если не найден ответ то будет -1
+})
+console.log('findIndexSalaries', findIndexSalaries)
+
+//Метод some, every - принимает в себя колл-бэк
+//some - возвращает true/false, возвращет true если найден хотя бы один элемент удовлетворяющий условию
+
+const someSalaries = salariesOfDevelopers.some((salary) => {
+    return salary > 1000 //true
+})
+console.log('someSalaries', someSalaries)
+
+const someSalaries1 = salariesOfDevelopers.some((salary) => {
+    return salary > 3000 //false
+})
+console.log('someSalaries1', someSalaries1)
+
+//every - возвращает true/false, возвращет true если все элементы удовлетворяют условию
+
+const everySalaries = salariesOfDevelopers.every((salary) => {
+    return salary > 0 //true
+})
+console.log('everySalaries', everySalaries)
+
+const everySalaries1 = salariesOfDevelopers.every((salary) => {
+    return salary > 3000 //false
+})
+console.log('everySalaries1', everySalaries1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
