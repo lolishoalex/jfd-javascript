f2a9d7af21bfacd18fac1e0a7c0dd095c5446a37
lolishoalex <shapilovka@gmail.com>
2022-08-10 20:21:27 +0200

    10 task 06

--- 10-objects/task-06/index.js
+++ 10-objects/task-06/index.js
@@ -1,3 +1,5 @@
+// 1й вариант решения - через итерацию people
+
 const people = [
    { id: 1, name: "Максим" },
    { id: 2, name: "Николай" },
@@ -8,6 +10,39 @@ const people = [
 const ordersArr = [4, 2, 1, 3];
 
 const giveTalonsInOrder = (patients, orders) => {
+    
+    const NewOrders = patients.map((patient) => {
+        let nameOrder = '';
+        console.log('patient', patient);//debug
+        orders.forEach(item => {
+            console.log('item', item);//debug
+            console.log('item === patient.id', item === patient.id);//debug
+            if (item === patient.id) {
+                nameOrder = patient.name
+                console.log('nameOrder', nameOrder);//debug
+            }
+        });
+        return {id: patient.id, name: nameOrder}
+    })
+    console.log('NewOrders',NewOrders)//debug
+
+    return orders.map((position) => {
+        console.log('position', position);//debug
+        return patients.find((patient) => {
+            console.log('patient', patient);//debug
+            return patient.id === position
+        })
+    })
+
+    //return orders.map(position => patients.find(patient => patient.id === position))
+
+}
+const result = giveTalonsInOrder(people, ordersArr);
+console.log('result', result);
+
+// 2й вариант решения -  через итерацию ordersArr
+
+/* const giveTalonsInOrder = (patients, orders) => {
         //берем очередь из массива orders, итерируем 
         //чтобы возвратить новый массив с данными из patients
         const NewOrders = orders.map((item) => {
@@ -24,7 +59,8 @@ const giveTalonsInOrder = (patients, orders) => {
     return NewOrders
 }
 const result = giveTalonsInOrder(people, ordersArr);
-console.log('result', result);
+console.log('result', result); */
+
 /* Возвращает массив
 [
    { id: 4, name: 'Виталий' },
@@ -33,3 +69,5 @@ console.log('result', result);
    { id: 3, name: 'Ангелина' }
 ]
 */
+
+//тут два варианта решения
