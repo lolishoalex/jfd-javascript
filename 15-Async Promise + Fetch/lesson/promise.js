13a73d546f65070aeb2ce6e718748ffc9fb1a941
lolishoalex <shapilovka@gmail.com>
2022-11-01 18:14:53 +0100

    15-Async Promise + Fetch

--- 15-Async Promise + Fetch/lesson/promise.js
+++ 15-Async Promise + Fetch/lesson/promise.js
@@ -6,6 +6,7 @@
 
 const developer = {
     name: 'Maxim',
+    //isJsDev: false,
     isJsDev: true,
 };
 
@@ -16,7 +17,7 @@ const developer = {
 //2 кол-во миллисекунд
 
 setTimeout(() => {
-    console.log('setTimeout');
+    //console.log('setTimeout');
 }, 3000);
 
 
@@ -27,5 +28,54 @@ console.log(developer);//проверка на асинхронность
 //но вызывается каждый раз через указанный интервал времени
 
 setInterval(() => {
-    console.log('setInterval');
+    //console.log('setInterval');
 }, 1000);
+
+
+
+//Promise - для создиния промиса необходимо указать 
+//new Promise(() => {})
+//коллбэк промиса принимает в себя 2 параметра: resolve, reject
+//resolve - это ф-я, которая вызывается если промис был выполнен успешно
+//reject - это ф-я, которая вызывается если промис был выполнен с ошибкой
+//У Promise есть три статуса:
+//pending - ожидание
+//fullfilled - выполнен
+//rejected - ошибка
+
+
+const promise = new Promise((resolve, reject) => {
+    if (developer.isJsDev) {
+        setTimeout(() => {
+            resolve(`${developer.name} is JavaScript developer`);
+        }, 3000);
+    } else {
+        reject(`${developer.name} is NOT JavaScript developer`)
+    }
+});
+
+console.log(promise);
+/* На этом этапе промис находится в pending статусе
+чтобы получить данные, которые мы передавали, необходимо 
+обработать промис.
+Для этого есть нметоды:
+then - принимает в себя коллбэк, получает данные промиса
+catch - принимает в себя коллбэк, для обработки ошибок промиса (reject)
+finally - принимает в себя коллбэк */
+
+promise
+    .then((successMassage) => {
+        console.log('successMassage:', successMassage);
+    })
+    .catch((error) => {
+        console.log('error:', error);
+    })
+    .finally(() => {
+        console.log('finally');
+    })
+/* этот then будет вызываться когда у нас будет выполняться resolve
+в случае если сработал reject, в консоли появляется ошибка,
+которая будет мешать дальнейшему выполнению кода, в этом случае 
+используем catch */
+/* finally - вызывается в последнюю очередь, при чем не важно произошла
+ошибка или нет */
