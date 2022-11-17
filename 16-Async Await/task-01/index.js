5b605d4052ec719ab131ae779b539e5873d436eb
lolishoalex <shapilovka@gmail.com>
2022-11-17 19:22:06 +0100

    16 task 01

---
+++ 16-Async Await/task-01/index.js
@@ -0,0 +1,37 @@
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+let isLoading = false
+const createNewPost = async () => {
+ isLoading = true
+    try {
+        const response = await fetch(POSTS_URL, {
+        method: 'POST',
+        })
+        const posts = await response.json()
+        console.log('posts', posts)
+    } catch (error){
+        console.log('error', error)
+    } finally {
+        isLoading = false
+    }
+}
+createNewPost()
+
+/* const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+let isLoading = false;
+const createNewPost = () => {
+ isLoading = true;
+ fetch(POSTS_URL, {
+   method: 'POST',
+ })
+   .then((response) => response.json())
+   .then((result) => {
+     console.log('result', result)
+   })
+   .catch((error) => {
+     console.log('error', error)
+   })
+   .finally(() => {
+     isLoading = false;
+   });
+};
+createNewPost(); */
