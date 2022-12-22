e6c73eea3997c57913dbe2b04b135e00ee6c546b
lolishoalex <shapilovka@gmail.com>
2022-12-22 17:49:51 +0100

    18 task 01

--- 18-Webpack-Rollup/task-01/index.js
+++ 18-Webpack-Rollup/task-01/index.js
@@ -1,1 +1,17 @@
+import "./index.css";
+import JS_IMAGE from './assets/16x9.png';
+
+const iLoveJS = document.createElement('h1')
+iLoveJS.className = 'iLoveJS-h1'
+iLoveJS.textContent = 'I love JavaScript'
+
+document.body.append(iLoveJS)
+
+const jSImageHtml = document.createElement('img')
+jSImageHtml.className = 'js-image'
+jSImageHtml.src = JS_IMAGE
+
+document.body.append(jSImageHtml)
+
+
 console.log('Hello World!')
