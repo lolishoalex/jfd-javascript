e7e8d6a21a24d289be674ac94f733a6696e7310a
lolishoalex <shapilovka@gmail.com>
2022-12-23 20:03:39 +0100

    19-ES6 import export

---
+++ 19-ES6 import export/lesson/src/modules/app.js
@@ -0,0 +1,19 @@
+import { JSBlock } from './js-block'
+import { TimerBlock } from './timer-block'
+import { JS_CREATION_DATE } from '../core/constants/settings'
+
+export default class App {
+    #JSBlock
+    #TimerBlock
+
+    constructor() {
+        this.#JSBlock = new JSBlock()
+        this.#TimerBlock = new TimerBlock(JS_CREATION_DATE)
+    }
+
+    run() {
+        const jsBlockHTML = this.#JSBlock.render()
+        const timerBlockHTML = this.#TimerBlock.render()
+        document.body.append(jsBlockHTML, timerBlockHTML)
+    }
+}
