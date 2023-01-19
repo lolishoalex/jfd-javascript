322dc8c56137d93905b5668d8940c7ffe080647b
lolishoalex <shapilovka@gmail.com>
2023-01-19 19:49:40 +0100

    21 task 03

---
+++ 21-Map-Set/task-03/index.js
@@ -0,0 +1,12 @@
+/* let map = new Map();
+map.set("name", "John");
+let keys = map.keys();
+// Error: keys.push is not a function
+// Ошибка: keys.push -- это не функция
+keys.push("more"); */
+
+let map = new Map();
+map.set("name", "John");
+let keys = Array.from(map.keys());
+keys.push("more");
+alert(keys); // name, more
